9d1b91ce7fb48b0fc5ae11924665d7446c3578cf
Andrew <Andresfdez9@gmail.com>
2020-02-05 00:19:05 +0100

    SDI-2.2 Manejo de Sesiones

---
+++ src/com/uniovi/sdi/ServletCarrito.java
@@ -0,0 +1,103 @@
+package com.uniovi.sdi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletCarrito
+ */
+@WebServlet("/incluirEnCarrito")
+public class ServletCarrito extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletCarrito() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * 
+	 * Obtenemos el carrito guardado en sesión.Si no hay carrito, se trata de un nuevo usuario
+	 * entonces instanciamos un nuevo carrito y lo insertamos en sesión.
+	 * 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session=request.getSession();
+		HashMap<String,Integer> carrito =
+		(HashMap<String,Integer>) request.getSession().getAttribute("carrito");
+		// No hay carrito, creamos uno y lo insertamos en sesión
+		if (carrito == null) {
+		carrito = new HashMap<String,Integer>();
+		 request.getSession().setAttribute("carrito", carrito);
+		}
+		String producto = request.getParameter("producto");
+		if (producto != null){
+		insertarEnCarrito(carrito, producto);
+		}
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println(carritoEnHTML(carrito)+"<br>");
+		out.println("<a href=\"tienda.html\">Volver</a></BODY></HTML>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	/**
+	 * Insertamos nombre del producto(clave) y número de unidades(valor) en el carrito
+	 * 
+	 * @param carrito
+	 * @param claveProducto
+	 */
+	private void insertarEnCarrito(Map<String,Integer> carrito, String claveProducto) {
+		
+		if(carrito.get(claveProducto) == null)
+			carrito.put(claveProducto,new Integer(1));
+		else {
+			int numeroArticulos = carrito.get(claveProducto).intValue();
+			carrito.put(claveProducto, new Integer (numeroArticulos+1));
+		}
+	       
+	}
+	/**
+	 * Devuelve una cadena con todos los elementos del carrito
+	 * 
+	 * @param carrito
+	 * @return
+	 */
+	private String carritoEnHTML (Map<String,Integer> carrito) {
+		
+		String carritoEnHTML="";
+		
+		for(String key:carrito.keySet())
+			carritoEnHTML += "<p>[" +key+"]" + carrito.get(key) + " unidades</p>";
+		return carritoEnHTML;
+		
+		
+	}
+	
+
+}
